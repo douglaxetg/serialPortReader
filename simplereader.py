455037e6818aeeded090e792e8d3ba31417bbe12
douglaxetg <douglastovar.g@gmail.com>
2022-03-30 01:01:14 -0400

    main class and connection function

--- simplereader.py
+++ simplereader.py
@@ -1,3 +1,22 @@
 # -*- coding: cp1252 -*-
 import threading
 import serial
+
+class simpleReader():
+    def __init__(self, port, baud):
+        self.port = port
+        self.baud = baud
+
+    def connection(self):
+        try:
+            self.connection = serial.Serial(self.port, self.baud)
+            print("Connected")
+        except:
+            print("Connection failed")
+
+linuxPort = "/dev/ttyUSB0"
+windowsPort = "COM5"
+appMonitor = simpleReader(linuxPort, 115200)
+appMonitor.connection()
+while True:
+    pass
